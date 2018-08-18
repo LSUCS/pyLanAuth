931eb99862556a15515c096f1d223937149d8b9f
Zack Pollard <zackpollard@ymail.com>
2018-08-18 04:42:38 +0100

    Commit new and old lan-auth scripts

---
+++ scripts/lan-auth.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Add a user's IP address to the EdgeOS Firewall
+# e.g. ./lan-auth.sh 48 oliverw92 192.168.0.82
+echo "$@$@"
+
+
+################
+# START CONFIG #
+
+AUTH_GROUP=authed_lan_users
+
+#  END CONFIG  #
+################
+
+# Execute commands without running configure
+run=/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper
+
+# Go into config mode
+$run begin
+
+# Add users IP to firewall group
+$run firewall group address-group $AUTH_GROUP address $3
+echo "Added $3 to $AUTH_GROUP"
+
+# Commit
+$run commit
+$run end
